694bff9b1daa7a75cdf5e30cad9e387d80dde3db
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2021-05-23 20:13:02 +0900

    format terraform

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,18 +1,18 @@
 variable "standalone_ami_name" {
-  type = string
-  default = "isuconXX-qualify"
+  type        = string
+  default     = "isuconXX-qualify"
   description = "AMI Name of stand alone instance"
 }
 
 variable "standalone_ami_owner" {
-  type = string
-  default = "839726181030" # owner of https://github.com/matsuu/aws-isucon
+  type        = string
+  default     = "839726181030" # owner of https://github.com/matsuu/aws-isucon
   description = "Owner of the stand alone AMI"
 }
 
 variable "access_cidr_blocks" {
-  type = string
-  default = "0.0.0.0/0" # "0.0.0.0/0,255.255.255.255/32" のようにカンマ区切りで複数指定可
+  type        = string
+  default     = "0.0.0.0/0" # "0.0.0.0/0,255.255.255.255/32" のようにカンマ区切りで複数指定可
   description = "cidr for access"
 }
 
