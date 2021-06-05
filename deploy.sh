119a2a532941cf1fde960cffa4d08f0e46edd6df
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2021-06-06 00:06:46 +0900

    for mysql conf

--- deploy.sh
+++ deploy.sh
@@ -18,18 +18,17 @@ sudo systemctl daemon-reload
 
 echo 'Updating config file...'
 sudo cp "$DIR/nginx.conf" /etc/nginx/nginx.conf
-# sudo cp "$HOME/redis.conf" /etc/redis/redis.conf
-# sudo cp "$HOME/my.conf" /etc/mysql/my.cnf
+# sudo cp "$DIR/redis.conf" /etc/redis/redis.conf
+sudo cp "$DIR/mysqld.cnf" /etc/mysql/mysql.conf.d/mysqld.cnf
 echo 'Updated config file!'
 
 echo 'Restarting services...'
 # sudo systemctl restart redis.service
-# Save cache
-# sudo systemctl restart mysql.service
+sudo systemctl restart mysql.service
 sudo systemctl restart nginx.service
 echo 'Restarted!'
 
 echo 'Rotating files'
 sudo bash -c 'cp /var/log/nginx/access.log /var/log/nginx/access.log.$(date +%s) && echo > /var/log/nginx/access.log'
-# sudo bash -c 'cp /var/lib/mysql/mysql-slow.log /var/lib/mysql/mysql-slow.log.$(date +%s) && echo > /var/lib/mysql/mysql-slow.log'
+sudo bash -c 'cp /tmp/mysql-slow.sql /tmp/mysql-slow.sql.$(date +%s).$(git rev-parse HEAD) && echo > /tmp/mysql-slow.sql'
 echo 'Rotated!'
